53ace251c1db1238cbb191419c7d57289eccb768
Mohamed EL-Mesery <Mohamedelmesery@std.mans.edu.eg>
2025-03-26 18:37:33 +0200

    update hyper tuning and add cross validation

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 from src.exception import CustomException
 
@@ -23,33 +23,44 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
-        report = {}
+        best_model_name = None
+        best_model_score = -np.inf
+        best_model = None
+        best_test_score = None
 
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+        for model_name, model in models.items():
+            para = param.get(model_name, {})
 
-            gs = GridSearchCV(model,para,cv=3)
-            gs.fit(X_train,y_train)
+            # GridSearchCV for hyperparameter tuning
+            gs = GridSearchCV(model, para, cv=3, n_jobs=-1)
+            gs.fit(X_train, y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            # Set the best parameters to the model
+            best_model_candidate = gs.best_estimator_
 
-            #model.fit(X_train, y_train)  # Train model
+            # Cross-validation score
+            cv_scores = cross_val_score(best_model_candidate, X_train, y_train, cv=5, scoring='r2', n_jobs=-1)
+            mean_cv_score = cv_scores.mean()
 
-            y_train_pred = model.predict(X_train)
+            # Train the model with best parameters
+            best_model_candidate.fit(X_train, y_train)
 
-            y_test_pred = model.predict(X_test)
+            # Predictions
+            y_test_pred = best_model_candidate.predict(X_test)
 
-            train_model_score = r2_score(y_train, y_train_pred)
-
+            # R2 scores
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            # Check if this model is the best
+            if mean_cv_score > best_model_score:
+                best_model_score = mean_cv_score
+                best_model_name = model_name
+                best_model = best_model_candidate
+                best_test_score = test_model_score
 
-        return report
+        return best_model_name, best_model_score, best_test_score, best_model
 
     except Exception as e:
         raise CustomException(e, sys)
